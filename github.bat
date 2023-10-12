136e935091b669eb7689356167c4da3d8fc3d9e1
Mamoun.Awad <mxawad2000@gmail.com>
2023-10-12 15:01:39 +0400

    initial commit

--- github.bat
+++ github.bat
@@ -10,9 +10,11 @@ REM Point Git To Your Existing Repo URL
 set URL="https://github.com/mxawad2000/sweb451_fall2022.git"
 git remote add origin %URL%
 
+git commit -m "initial commit"
+
 REM Verify That Your Repo Is Connected
 git remote -v
 REM Push Changes To GitHub Repo
 git push -u origin spaceship
-
+git commit -m "initial commit"
 
